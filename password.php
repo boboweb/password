840ce1414403935572bd35989bec844247312953
boboweb <boboweb@gmail.com>
2015-08-31 10:19:44 +0800

    update password.php

--- password.php
+++ password.php
@@ -1,2 +1,68 @@
-<?php
-include 'index.html';
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="chrome=1">
+    <title>Password by boboweb</title>
+
+    <link rel="stylesheet" href="stylesheets/styles.css">
+    <link rel="stylesheet" href="stylesheets/github-dark.css">
+    <script src="javascripts/scale.fix.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+
+    <!--[if lt IE 9]>
+    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <style>
+        section > form {
+            padding: 30px;
+        }
+
+        section > form > p {
+            text-align: center;
+        }
+
+        .input {
+            width: 90%;
+            line-height: 2;
+            font-size: 14px;
+            padding: 3px 5px;
+        }
+
+        .btn {
+            line-height: 1.8;
+            font-size: 14px;
+            padding: 3px 10px;
+        }
+    </style>
+</head>
+<body>
+<div class="wrapper">
+    <header>
+        <h1>Password</h1>
+
+        <p>Password Generator</p>
+
+        <p class="view"><a href="https://github.com/boboweb/password">View the Project on GitHub
+                <small>boboweb/password</small>
+            </a></p>
+        <ul>
+            <li><a href="https://github.com/boboweb/password/zipball/master">Download <strong>ZIP File</strong></a></li>
+            <li><a href="https://github.com/boboweb/password/tarball/master">Download <strong>TAR Ball</strong></a></li>
+            <li><a href="https://github.com/boboweb/password">View On <strong>GitHub</strong></a></li>
+        </ul>
+    </header>
+    <section>
+        <?php print_r($_POST) ?>
+    </section>
+</div>
+<footer>
+    <p>Project maintained by <a href="https://github.com/boboweb">boboweb</a></p>
+
+    <p>Hosted on GitHub Pages &mdash; Theme by <a href="https://github.com/orderedlist">orderedlist</a></p>
+</footer>
+<!--[if !IE]>
+<script>fixScale(document);</script><![endif]-->
+
+</body>
+</html>
